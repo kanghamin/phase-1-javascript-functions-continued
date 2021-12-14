87e15f8b009501306f7393751397f79c4a8c20a5
kanghamin <gogokhm1@gmail.com>
2021-12-14 10:04:37 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+function saturdayFun(arg) {
+    if (arg) {
+        return `This Saturday, I want to ${arg}!`
+    } else {
+        return "This Saturday, I want to roller-skate!"
+    }
+}
+
+function mondayWork(arg) {
+    if (arg) {
+        return `This Monday, I will ${arg}.`
+    } else {
+        return "This Monday, I will go to the office."
+    }
+}
+
+function wrapAdjective(symbol) {
+   return function result(word) {
+        if (symbol === "*") {
+            return `You are ${symbol}${word}${symbol}!`
+        } else {
+            return `You are ${symbol}${word}${symbol}!`
+        }
+    }
+}
+
